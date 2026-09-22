e8e72ec1cee51daa6cf1c549e6a9ddc999830176
SannoBR <56984690+Vuthos@users.noreply.github.com>
2026-09-22 20:38:05 -0300

    chore: label community submissions as v1.3.0

--- src/online/community.js
+++ src/online/community.js
@@ -17,7 +17,7 @@ function buildCommunityIssueUrl(shareUrl,alias){
     '**Formato:** SAN Map v7',
     '**Link:** '+shareUrl,
     '',
-    '> Envio criado pelo SandBox Editor v1.2.2.'
+    '> Envio criado pelo SandBox Editor v1.3.0.'
   ].join('\n');
   const u=new URL(ONLINE_CONFIG.issuesNewUrl);
   u.searchParams.set('title',title);
